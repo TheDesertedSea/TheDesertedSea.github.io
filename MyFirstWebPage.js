6267b1db17a36f100177e196774e92a428c65afd
TheDesertedSea <745468443@qq.com>
2020-04-03 13:30:47 +0800

    blog change4.3

---
+++ MyFirstWebPage.js
@@ -0,0 +1,10 @@
+function displayDate (){
+    document.getElementById("kk").innerHTML=Date();
+}
+function matchif(){
+    x=document.getElementById("wannabe");
+    
+}
+window.alert("hello");
+document.write("<p>hape</p>");
+document.write(Date());
